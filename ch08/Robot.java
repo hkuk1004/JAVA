0808ad70f24a1bb21dc895a4a54da539888275f5
hkuk1004 <79433726+hkuk1004@users.noreply.github.com>
2021-04-05 12:26:30 +0900

    Add files via upload

---
+++ ch08/Robot.java
@@ -0,0 +1,19 @@
+package ch08;
+public class Robot {}
+class DanceRobot extends Robot {
+	void dance() {
+		System.out.println("√„¿ª √‰¥œ¥Ÿ");
+	}
+}
+
+class SingRobot extends Robot {
+	void sing() {
+		System.out.println("≥Î∑°∏¶ ∫Œ∏®¥œ¥Ÿ");
+	}
+}
+
+class DrawRobot extends Robot {
+	void draw() {
+		System.out.println("±◊∏≤¿ª √‰¥œ¥Ÿ");
+	}
+}
---
+++ ch08/RobotEx.java
@@ -0,0 +1,22 @@
+package ch08;
+
+public class RobotEx {
+	public static void action(Robot r) {
+		if (r instanceof DanceRobot) ((DanceRobot)r).dance();
+		else if (r instanceof SingRobot) ((SingRobot)r).sing();
+		else if (r instanceof DrawRobot) {
+			DrawRobot dr = (DrawRobot)r;
+			dr.draw();
+		}
+	}
+	public static void main(String[] args) {
+		Robot[] robots = new Robot[3];
+		robots[0] = new DanceRobot();
+		robots[1] = new SingRobot();
+		robots[2] = new DrawRobot();
+		
+		for(Robot robot : robots) {
+			action(robot);
+		}
+	}
+}
