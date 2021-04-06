fb800d9c31d0a5a62632291547b7831c639caa41
hkuk1004 <79433726+hkuk1004@users.noreply.github.com>
2021-04-06 16:47:28 +0900

    Add files via upload

---
+++ ch11/Array3.java
@@ -0,0 +1,26 @@
+package ch11;
+
+import java.util.ArrayList;
+
+public class Array3 {
+		public static void main(String[] args) {
+			ArrayList<String> list = new ArrayList<>();
+			// Array3 ±¸··ÀÌ, ÆÈ··ÀÌ, Ä¥··ÀÌ, À°··ÀÌ, ±¸··ÀÌ µ¥ÀÌÅÍÃß°¡ÇÏ°í Ãâ·Â
+			list.add("±¸··ÀÌ");list.add("ÆÈ··ÀÌ");list.add("Ä¥··ÀÌ");
+			list.add("À°··ÀÌ");list.add("±¸··ÀÌ"); print(list);
+			// ÀÎµ¦½º 1¹ø¿¡ ±¸··»ï Ãß°¡ÇÏ°í Ãâ·Â
+			list.add(1,"±¸··»ó"); print(list);
+			// ÀÎµ¦½º 3¹ø¿¡ µµ·æ·æÀ¸·Î ¼öÁ¤ÇÏ°í Ãâ·Â
+			list.set(3,"µµ·æ·æ"); print(list);
+			// ÀÎµ¦½º 2¹ø µ¥ÀÌÅÍ Áö¿ì°í Ãâ·Â
+			list.remove(2); print(list);
+			// ±¸··ÀÌ Áö¿ì°í Ãâ·Â
+			list.remove("±¸··ÀÌ"); print(list);
+		}
+		private static void print(ArrayList<String> list) {
+			for(String snake : list) {
+				System.out.print(snake+"\t");
+			}
+			System.out.println();
+		}
+}
