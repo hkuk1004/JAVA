5faec611f6850c042cf751f3b4a2de0350253a61
hkuk1004 <79433726+hkuk1004@users.noreply.github.com>
2021-04-08 12:27:53 +0900

    Add files via upload

--- ch11/customer/CustomerDaoImpl.java
+++ ch11/customer/CustomerDaoImpl.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class CustomerDaoImpl {  // DAO Data Access Object
 	private Map<String, Customer> map = new HashMap<>(); // key는 id, 값 고객정보
 	public Customer select(String id) {
-		return map.get(id);  // get(Key)
+		return map.get(id);  // get(Key): 결과는 값 customer객체
 	}
 	public int insert(Customer customer) {
 		map.put(customer.getId(), customer);
@@ -14,5 +14,13 @@ public class CustomerDaoImpl {  // DAO Data Access Object
 	}
 	public Collection<Customer> list() {
 		return map.values();
+	}
+	public int update(Customer customer) {
+		map.put(customer.getId(), customer); // key가 같으면 덮어쓴다 => 수정
+		return 1;
+	}
+	public int delete(String id) {
+		map.remove(id);
+		return 1;
 	}
 }
--- ch11/customer/Ex01.java
+++ ch11/customer/Ex01.java
@@ -17,6 +17,9 @@ public class Ex01 {
 				insert(command.split(","));
 			else if (command.startsWith("update"))
 				update(command.split(","));
+			// delete,id 삭제 데이터 읽어서 있으면 삭제, 없으면 "없는데 삭제 못하지" remove
+			else if (command.startsWith("delete"))
+				delete(command.split(","));
 			else if (command.startsWith("select")) 
 				select(command.split(","));
 			else if (command.equals("list"))
@@ -26,9 +29,30 @@ public class Ex01 {
 		sc.close();
 		System.out.println("프로그램 종료");
 	}
-	private static void update(String[] split) {
-		// TODO Auto-generated method stub
-		
+	private static void delete(String[] str) {
+		int k = lengthChk(str.length, 2);
+		if (k==0) return;
+//		if (str.length != 2) {// select,id
+//			help();
+//			return; // 메서드 종료
+//		}
+		int result = cs.delete(str[1]);
+		if (result > 0) System.out.println("삭제 성공 ㅋㅋ");
+	}
+	private static void update(String[] str) {
+		int k = lengthChk(str.length, 6);
+		if (k==0) return;
+//		if (str.length != 6) {
+//			help();
+//			return; // 메서드 종료
+//		}
+		Customer customer=new Customer(str[1],str[2], str[4],str[5],new Date());
+		if (!str[2].equals(str[3])) {
+			System.out.println("암호와 암호확인이 다릅니다");
+			return;
+		}
+		int result = cs.update(customer); // result는 성공 여부 체크(1은 성공, 0은 실패)
+		if (result > 0) System.out.println("수정 성공 !! 대박");
 	}
 	private static void list() {
 		Collection<Customer> list = cs.list();
@@ -39,21 +63,33 @@ public class Ex01 {
 				System.out.println(customer);
 			}
 	}
-	private static void select(String[] str) {
-		if (str.length != 2) {// select,id
+	static int lengthChk(int length, int num) {
+		int result = 1;
+		if (length != num) {// select,id
 			help();
-			return; // 메서드 종료
+			result = 0;
 		}
+		return result;
+	}
+	private static void select(String[] str) {
+		int k = lengthChk(str.length, 2);
+		if (k==0) return;
+//		if (str.length != 2) {// select,id
+//			help();
+//			return; // 메서드 종료
+//		}
 		// id가 일치하는 고객 데이터를 읽어서 고객 객체를 생성하고 저장
 		Customer customer = cs.select(str[1]);
 		if (customer == null) System.out.println("없는 고객입니다");
 		else System.out.println(customer);
 	}
 	private static void insert(String[] str) {
-		if (str.length != 6) {
-			help();
-			return; // 메서드 종료
-		}
+		int k = lengthChk(str.length, 6);
+		if (k==0) return;
+//		if (str.length != 6) {
+//			help();
+//			return; // 메서드 종료
+//		}
 		Customer customer=new Customer(str[1],str[2], str[4],str[5],new Date());
 		if (!str[2].equals(str[3])) {
 			System.out.println("암호와 암호확인이 다릅니다");
@@ -68,6 +104,7 @@ public class Ex01 {
 		System.out.println("다음 명령어에서 선택 하세요");
 		System.out.println("insert,id,암호,암호확인,이메일,이름");
 		System.out.println("update,id,암호,암호확인,이메일,이름");
+		System.out.println("delete,id");
 		System.out.println("select,id");
 		System.out.println("list");
 		System.out.println("exit");
