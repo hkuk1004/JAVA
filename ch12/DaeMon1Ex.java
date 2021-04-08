a85b5685e57e50d34cfa50c804c7031ee33332ee
hkuk1004 <79433726+hkuk1004@users.noreply.github.com>
2021-04-08 15:33:37 +0900

    Add files via upload

--- ch12/DaeMon1Ex.java
+++ ch12/DaeMon1Ex.java
@@ -11,6 +11,8 @@ class DaeMon1 extends Thread{
 public class DaeMon1Ex {
 	public static void main(String[] args) {
 		DaeMon1 dm = new DaeMon1();
+		// 메인 작업이 끝나면 모든 쓰레드도 종료하라, start( ) 메서드 앞에 있어야 한다
+		dm.setDaemon(true);
 		dm.start();
 		for(int i=1; i<20; i++) {
 			System.out.println(i);
