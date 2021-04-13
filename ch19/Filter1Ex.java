ee5d1cd2d14eae4fd42a154dcc1d9c8327222d71
hkuk1004 <79433726+hkuk1004@users.noreply.github.com>
2021-04-13 12:32:43 +0900

    Add files via upload

---
+++ ch19/Filter1Ex.java
@@ -0,0 +1,15 @@
+package ch19;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Filter1Ex {
+	public static void main(String[] args) {
+		List<String> names = Arrays.asList("È«±æµ¿","½Å¿ë±Ç","°©ÀÚ¹Ù","½Å¿ë±Ç","½Å¹ÎÃ¶");
+		names.stream().distinct().forEach(n->System.out.println(n));
+		System.out.println("================");
+		names.stream().filter(n->n.startsWith("½Å")).forEach(n->System.out.println(n));
+		System.out.println("================");
+		names.stream().distinct().filter(n->n.startsWith("½Å")).forEach(n->System.out.println(n));
+	}
+}
