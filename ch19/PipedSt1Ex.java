e6a26b611f3a62d21a7781c015b92568195b32fe
hkuk1004 <79433726+hkuk1004@users.noreply.github.com>
2021-04-13 12:26:11 +0900

    Add files via upload

---
+++ ch19/PipedSt1Ex.java
@@ -0,0 +1,24 @@
+package ch19;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PipedSt1Ex {
+	public static void main(String[] args) {
+		List<Member> list = new ArrayList<>();
+		list.add(new Member("세정",23, Member.FEMALE));
+		list.add(new Member("원빈",41, Member.MALE));
+		list.add(new Member("하니",27, Member.FEMALE));
+		list.add(new Member("현빈",38, Member.MALE));
+		list.add(new Member("아이유",23, Member.FEMALE));
+		
+		double avg =  list.stream().mapToInt(Member::getAge).average().getAsDouble();
+		System.out.println("평균나이 : "+avg);
+		avg = list.stream().filter(m->m.getGender()==Member.MALE)
+				.mapToInt(Member::getAge).average().getAsDouble();
+		System.out.println("남자평균나이 : " + avg);
+		avg = list.stream().filter(m->m.getGender()==Member.FEMALE)
+				.mapToInt(Member::getAge).average().getAsDouble();
+		System.out.printf("여자평균나이 : %.2f",avg);
+	}
+}
