5c2e5f1c39c2c082b1282ca28d918206de5aa8f8
hkuk1004 <79433726+hkuk1004@users.noreply.github.com>
2021-04-13 10:46:08 +0900

    Add files via upload

---
+++ ch18/Lamda1Ex.java
@@ -0,0 +1,22 @@
+package ch18;
+interface MyFunction1 {
+	public void method();
+}
+class MyFun1Class implements MyFunction1{
+	@Override
+	public void method() {
+		System.out.println("난 재정의한 클래스");
+	}
+}
+public class Lamda1Ex {
+	public static void main(String[] args) {
+		MyFunction1 mc1 = new MyFun1Class();
+		mc1.method();
+		mc1 = new MyFunction1() { // 무명(익명) 클래스
+			@Override
+			public void method() {
+				System.out.println("난 인터페이스 직접 재정의");
+			}
+		};
+	}
+}
